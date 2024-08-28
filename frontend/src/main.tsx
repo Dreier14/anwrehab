1de8b6ec28b01254d0ec11fe637df04175dc45f0
Adam Dreier <adreier2@asu.edu>
2024-08-28 12:40:59 -0700

    Updated Code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
